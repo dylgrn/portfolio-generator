218a38e5a70c0af16602e70b885729062328be7c
dylan green <dylgrn.github.io>
2021-05-14 14:59:10 -0500

    webpage generates from app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-//console.log(profileDataArgs);
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-    console.log(profileDataArr[i])
-    }
+const [name, github] = profileDataArgs;
 
-    console.log('=================');
+const fs = require('fs');
 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-printProfileData(profileDataArgs);
+const generatePage = require('./src/page-template.js')
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!')
+})
