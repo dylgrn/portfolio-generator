b3b39b5ecdbb6fc9f6f0754f2f69ece7868d3f2a
dylan green <dylgrn.github.io>
2021-05-14 16:21:29 -0500

    inquierer added

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const inquirer = require('inquirer');
+// const [name, github] = profileDataArgs;
 
-const [name, github] = profileDataArgs;
+// const fs = require('fs');
 
-const fs = require('fs');
+// const generatePage = require('./src/page-template.js')
 
-const generatePage = require('./src/page-template.js')
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//     if (err) throw err;
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw err;
-
-    console.log('Portfolio complete! Check out index.html to see the output!')
-})
+//     console.log('Portfolio complete! Check out index.html to see the output!')
+// })
+inquirer
+.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }
+])
+.then(answers => console.log(answers));
